d9da125b46327f2c04c3db7244ca486530c217f2
Daniel Rosowski <rosowski@smartsquare.de>
2018-07-12 15:20:52 +0200

    Adapted the serverless-chrome examples to consume events from dynamodb

--- src/handlers/screenshot.js
+++ src/handlers/screenshot.js
@@ -1,35 +1,40 @@
 import log from '../utils/log'
 import screenshot from '../chrome/screenshot'
+var AWS = require("aws-sdk");
+var s3 = new AWS.S3();
 
 export default async function handler (event, context, callback) {
-  const {
-    queryStringParameters: {
-      url = 'https://github.com/adieuadieu/serverless-chrome',
-      mobile = false,
-    },
-  } = event
+  event.Records.forEach(async (record) => {
+    if (record.eventName == 'INSERT') {
+      const url = record.dynamodb.NewImage.url.S;
+      const websiteId = record.dynamodb.NewImage.websiteId.S;
+      const hashValue = record.dynamodb.NewImage.hashValue.N;
 
-  let data
+      let data
+      log('Processing screenshot capture for', url)
+      const startTime = Date.now()
 
-  log('Processing screenshot capture for', url)
+      try {
+        data = await screenshot(url, false)
+      } catch (error) {
+        console.error('Error capturing screenshot for', url, error)
+        return callback(error)
+      }
 
-  const startTime = Date.now()
-
-  try {
-    data = await screenshot(url, mobile)
-  } catch (error) {
-    console.error('Error capturing screenshot for', url, error)
-    return callback(error)
-  }
-
-  log(`Chromium took ${Date.now() - startTime}ms to load URL and capture screenshot.`)
-
-  return callback(null, {
-    statusCode: 200,
-    body: data,
-    isBase64Encoded: true,
-    headers: {
-      'Content-Type': 'image/png',
-    },
-  })
-}
+      log(`Chromium took ${Date.now() - startTime}ms to load URL and capture screenshot.`)
+      const key = `${websiteId}-${Math.abs(hashValue)}.png`
+      const params = {
+        Bucket : "wecky-screens",
+        Key : key,
+        Body : new Buffer(data, 'base64')
+      }
+      s3.putObject(params, function(err, data) {
+        if (err) console.log(err, err.stack);
+      });
+      log(`Successfully uploaded ${key} to bucket wecky-screens`)
+    } else {
+      log('Skipping dynamodb non insert');
+    }
+  });
+  return callback(null, "Processed all dynamodb events");
+};
